1edcb2946d890641c4cfa07b7dd8284b0f3a89ca
AReesuno <anthonymresano@su.edu.ph>
2024-11-20 19:22:13 +0800

    Add files via upload

---
+++ ResanoE5-3.js
@@ -0,0 +1,34 @@
+function replaceMe() {
+    let storT = document.getElementById('story').value;
+    let findT = document.getElementById('findText').value;
+    let replT = document.getElementById('replaceText').value;
+    let newText = storT.replace(new RegExp(findT, 'g'), replT);
+    document.getElementById('story').value = newText;
+}
+
+function clearMe() {
+    document.getElementById('findText').value = "";
+    document.getElementById('replaceText').value = "";
+    console.log('clear');
+}
+
+function upperC() {
+    let storT = document.getElementById('story').value;
+    document.getElementById('story').value = storT.toUpperCase();
+}
+
+function lowerC() {
+    let storT = document.getElementById('story').value;
+    document.getElementById('story').value = storT.toLowerCase();
+}
+
+function grabLength() {
+    let textLength = document.getElementById('story').value.length;
+    document.getElementById('lengthDisplay').innerText = "Length of text: " + textLength + " characters";
+}
+
+function setTheme(theme) {
+    let body = document.body;
+    body.className = ""; 
+    body.classList.add(theme);
+}
